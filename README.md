b8637ce4ff33240fdce1c23c6e7ded564f5d998f
Martin Schupfner <k204212@mistralpp2.hpc.dkrz.de>
2020-10-02 15:23:10 +0200

    Added two notebooks to demonstrate xESMF regridding from a regular grid and instructions to create a fitting environment.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # regrid-prototype
 Regridding prototype for clisops developed in this (temporary) repository.
+
+Contains yet only jupyter notebooks that have the aim to test the regridding functionality of xESMF with different grids of CMIP6 models.
+To setup the environment see docs/notebooks/Create_Env_xESMF.sh
+
---
+++ docs/notebooks/Create_Env_xESMF.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# mistral, user related
+module unload netcdf_c
+module unload python
+module load python3/2020.02-gcc-9.1.0
+module load anaconda3
+
+# Clone xesmf to this directory
+xesmf_dir=xesmf_latest
+
+# Clone clisops to this directory
+clisops_dir=clisops
+
+# Name of the new environment
+envname=xesmf_test
+
+# Name of the new JupyterNB Kernel
+kernel_name="new_kernel"
+
+
+
+
+# Create new conda environment and install xESMF incl dependencies
+conda create -n $envname python=3
+source activate $envname
+conda install -c conda-forge esmpy
+#pip install xesmf
+git clone https://github.com/pangeo-data/xESMF.git $xesmf_dir
+git checkout tags/v0.4.0 -b latest
+pip install pytest dask toolz
+cd $xesmf_dir
+python setup.py install
+
+# Test the installation
+python -m pytest
+#pytest -v --pyargs xesmf
+
+# Install clisops and dependencies
+cd ..
+mkdir -p $clisops_dir
+git clone https://github.com/roocs/clisops.git $clisops_dir
+cd $clisops_dir
+conda-env update -n $envname -f environment.yml 
+pip install -r requirements_dev.txt 
+conda install -c conda-forge cartopy ipykernel jupyterlab udunits2=2.2
+git submodule update --init
+python setup.py develop
+
+# Optionally install pyesgf and intake-esm (the latter needed for the notebooks)
+conda install -c conda-forge intake-esm 
+pip install esgf-pyclient
+
+# Install the environment as Kernel for Jupyter
+python -m ipykernel install --user --name "$kernel_name" --display-name="$kernel_name"
+
+# Final test
+pytest -v tests
+cd ../$xesmf_dir
+#pytest -v --pyargs xesmf
+python -m pytest
