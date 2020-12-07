c3e94e113c809fc76fa08e277bd21f6ac098692d
sol1105 <martin.schupfner@web.de>
2020-12-07 14:47:58 +0100

    target_grids: added grids from Copernicus 34d project provided by SantanderMetGroup/ATLAS | xESMF_test_Adaptive_Masking-MPIOM.ipynb: Added threshold-factor for adaptive masking and the further testcases

--- docs/notebooks/Create_Env_xESMF.sh
+++ docs/notebooks/Create_Env_xESMF.sh
@@ -7,7 +7,7 @@ module load python3/2020.02-gcc-9.1.0
 module load anaconda3
 
 # Clone xesmf to this directory
-xesmf_dir=xesmf_latest
+xesmf_dir=xesmf
 
 # Clone clisops to this directory
 clisops_dir=clisops
@@ -24,32 +24,31 @@ kernel_name="new_kernel"
 # Create new conda environment and install xESMF incl dependencies
 conda create -n $envname python=3
 source activate $envname
-conda install -c conda-forge esmpy
+conda install -c conda-forge esmpy xarray scipy dask netCDF4 matplotlib cartopy jupyterlab
 #pip install xesmf
 git clone https://github.com/pangeo-data/xESMF.git $xesmf_dir
-git checkout tags/v0.4.0 -b latest
-pip install pytest dask toolz
+#pip install pytest dask toolz
 cd $xesmf_dir
-python setup.py install
+#git checkout tags/v0.5.0 -b latest
+pip install -e .
+#python setup.py develop
 
 # Test the installation
-python -m pytest
+#python -m pytest
 #pytest -v --pyargs xesmf
 
 # Install clisops and dependencies
 cd ..
-mkdir -p $clisops_dir
 git clone https://github.com/roocs/clisops.git $clisops_dir
 cd $clisops_dir
 conda-env update -n $envname -f environment.yml 
 pip install -r requirements_dev.txt 
-conda install -c conda-forge cartopy ipykernel jupyterlab udunits2=2.2
 git submodule update --init
 python setup.py develop
 
 # Optionally install pyesgf and intake-esm (the latter needed for the notebooks)
-conda install -c conda-forge intake-esm 
 pip install esgf-pyclient
+conda install -c conda-forge ipykernel intake-esm=2020.8.15
 
 #Plotting curvilinear grids, UGRID
 #pip install psy-maps
@@ -63,6 +62,6 @@ python -m ipykernel install --user --name "$kernel_name" --display-name="$kernel
 
 # Final test
 pytest -v tests
-cd ../$xesmf_dir
+cd ../$xesmf_dir/xesmf
 #pytest -v --pyargs xesmf
 python -m pytest
